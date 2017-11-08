ae2580738e3b034fbe80346294be352aefd11390
tom <tom.block@brockmann-consult.de>
2017-11-08 16:30:30 +0100

    intermediate

--- test/dmi/sst/mw_oe/preprocessor_test.py
+++ test/dmi/sst/mw_oe/preprocessor_test.py
@@ -26,3 +26,22 @@ class PreprocessorTest(unittest.TestCase):
 
         variable = prep_data.variables["insitu.lon"]
         self.assertEqual((7,), variable.shape)
+
+    def test_run_fetch_center_pixel_variables(self):
+        dataset = xr.Dataset()
+
+        data = DefaultData.create_default_array_3d(5, 5, 11, np.int32)
+        dataset["amsre.nwp.log_surface_pressure"] = Variable(["matchup_count", "ny", "nx"], data)
+
+        data = DefaultData.create_default_array_3d(5, 5, 11, np.float32)
+        dataset["amsre.pixel_data_quality23V"] = Variable(["matchup_count", "ny", "nx"], data)
+
+        preprocessor = Preprocessor()
+        prep_data = preprocessor.run(dataset)
+
+        # @todo 1 tb/tb continue here 2017-11-07
+        # variable = prep_data.variables["amsre.nwp.log_surface_pressure"]
+        # self.assertEqual((11,), variable.shape)
+        #
+        # variable = prep_data.variables["amsre.pixel_data_quality23V"]
+        # self.assertEqual((11,), variable.shape)
