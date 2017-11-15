0c704264f47e003c00656a2f69e4c7cb3e024daf
tom <tom.block@brockmann-consult.de>
2017-11-15 13:51:52 +0100

    added extract_center pro-processing

--- dmi/sst/mw_oe/preprocessor.py
+++ dmi/sst/mw_oe/preprocessor.py
@@ -3,6 +3,10 @@ import xarray as xr
 
 class Preprocessor:
     TO_SQUEEZE_NAMES = ["insitu.time", "insitu.lat", "insitu.lon", "insitu.sea_surface_temperature", "insitu.sst_depth", "insitu.sst_qc_flag", "insitu.sst_track_flag]"]
+    TO_AVERAGE_NAMES = ["amsre.brightness_temperature6V", "amsre.brightness_temperature6H", "amsre.brightness_temperature10V", "amsre.brightness_temperature10H", "amsre.brightness_temperature18V",
+                        "amsre.brightness_temperature18H", "amsre.brightness_temperature23V", "amsre.brightness_temperature23H", "amsre.brightness_temperature36V", "amsre.brightness_temperature36H"]
+    TO_CENTER_EXTRACT_NAMES = ["amsre.nwp.seaice_fraction", "amsre.nwp.sea_surface_temperature", "amsre.nwp.10m_east_wind_component", "amsre.nwp.10m_north_wind_component",
+                               "amsre.nwp.skin_temperature", "amsre.nwp.log_surface_pressure", "amsre.nwp.cloud_liquid_water", "amsre.nwp.total_column_water_vapour", "amsre.nwp.total_precip"]
 
     def run(self, dataset):
 
@@ -11,6 +15,15 @@ class Preprocessor:
         for variable_name in dataset.variables:
             if variable_name in self.TO_SQUEEZE_NAMES:
                 preprocessed_data[variable_name] = dataset.variables[variable_name].squeeze()
+                continue
 
+            if variable_name in self.TO_AVERAGE_NAMES:
+                print(variable_name)
+                continue
+
+            if variable_name in self.TO_CENTER_EXTRACT_NAMES:
+                # @todo 3 tb/tb read dimensions from variable and dynamically calculate center column 2017-11-15
+                preprocessed_data[variable_name] = dataset.variables[variable_name][:, 3, 3].squeeze()
+                continue
 
         return preprocessed_data
--- test/dmi/sst/mw_oe/preprocessor_test.py
+++ test/dmi/sst/mw_oe/preprocessor_test.py
@@ -13,9 +13,11 @@ class PreprocessorTest(unittest.TestCase):
         dataset = xr.Dataset()
 
         data = DefaultData.create_default_array_3d(1, 1, 7, np.int32)
+        data[3, 0, 0] = 78
         dataset["insitu.time"] = Variable(["matchup_count", "ny", "nx"], data)
 
         data = DefaultData.create_default_array_3d(1, 1, 7, np.float32)
+        data[4, 0, 0] = 0.89
         dataset["insitu.lon"] = Variable(["matchup_count", "ny", "nx"], data)
 
         preprocessor = Preprocessor()
@@ -23,25 +25,30 @@ class PreprocessorTest(unittest.TestCase):
 
         variable = prep_data.variables["insitu.time"]
         self.assertEqual((7,), variable.shape)
+        self.assertEqual(78, variable.data[3])
 
         variable = prep_data.variables["insitu.lon"]
         self.assertEqual((7,), variable.shape)
+        self.assertAlmostEqual(0.89, variable.data[4], 7)
 
     def test_run_fetch_center_pixel_variables(self):
         dataset = xr.Dataset()
 
-        data = DefaultData.create_default_array_3d(5, 5, 11, np.int32)
+        data = DefaultData.create_default_array_3d(5, 5, 11, np.int16)
+        data[5, 3, 3] = 101
         dataset["amsre.nwp.log_surface_pressure"] = Variable(["matchup_count", "ny", "nx"], data)
 
-        data = DefaultData.create_default_array_3d(5, 5, 11, np.float32)
-        dataset["amsre.pixel_data_quality23V"] = Variable(["matchup_count", "ny", "nx"], data)
+        data = DefaultData.create_default_array_3d(5, 5, 11, np.float64)
+        data[6, 3, 3] = 1.02
+        dataset["amsre.nwp.total_column_water_vapour"] = Variable(["matchup_count", "ny", "nx"], data)
 
         preprocessor = Preprocessor()
         prep_data = preprocessor.run(dataset)
 
-        # @todo 1 tb/tb continue here 2017-11-07
-        # variable = prep_data.variables["amsre.nwp.log_surface_pressure"]
-        # self.assertEqual((11,), variable.shape)
-        #
-        # variable = prep_data.variables["amsre.pixel_data_quality23V"]
-        # self.assertEqual((11,), variable.shape)
+        variable = prep_data.variables["amsre.nwp.log_surface_pressure"]
+        self.assertEqual((11,), variable.shape)
+        self.assertEqual(101, variable.data[5])
+
+        variable = prep_data.variables["amsre.nwp.total_column_water_vapour"]
+        self.assertEqual((11,), variable.shape)
+        self.assertAlmostEqual(1.02, variable.data[6], 8)
