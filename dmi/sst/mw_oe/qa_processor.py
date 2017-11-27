3649e2445a98831ae426ea959771cb0fae300508
tom <tom.block@brockmann-consult.de>
2017-11-27 17:50:36 +0100

    added qa for BT delta and abs_windspeed

--- dmi/sst/mw_oe/qa_processor.py
+++ dmi/sst/mw_oe/qa_processor.py
@@ -1,9 +1,22 @@
 import numpy as np
 
+BT_MIN = 0.0
+BT_MAX = 320.0
+WS_ABS_MAX = 50.0
+WS_MIN = 0.0
+WS_MAX = 20.0
+LON_ABS_MAX = 180.0
+LAT_ABS_MAX = 90.0
+SZA_MIN = 0.0
+SZA_MAX = 180.0
+
 
 class QaProcessor():
-
     def run_qa(self, dataset):
+        self.run_qa_general(dataset)
+        self.run_qa_bt_delta(dataset)
+
+    def run_qa_general(self, dataset):
         flag_array = dataset["invalid_data"].data
         for variable_name in dataset.variables:
             variable_data = dataset[variable_name].data
@@ -14,23 +27,55 @@ class QaProcessor():
                 continue
 
             if "brightness_temperature" in variable_name:
-                local_mask = (variable_data <= 0.0) | (variable_data >= 320.0)
+                local_mask = (variable_data <= BT_MIN) | (variable_data >= BT_MAX)
                 flag_array = np.logical_or(flag_array, local_mask)
                 continue
 
+            # @todo 3 tb/tb remove this? seems to be redundant tb 2017-11-27
             if "wind_component" in variable_name:
-                local_mask = np.absolute(variable_data) > 50.0
+                local_mask = np.absolute(variable_data) > WS_ABS_MAX
+                flag_array = np.logical_or(flag_array, local_mask)
+                continue
+
+            if "abs_wind_speed" in variable_name:
+                local_mask = (variable_data < WS_MIN) | (variable_data > WS_MAX)
                 flag_array = np.logical_or(flag_array, local_mask)
                 continue
 
             if "longitude" in variable_name:
-                local_mask = np.absolute(variable_data) > 180.0
+                local_mask = np.absolute(variable_data) > LON_ABS_MAX
                 flag_array = np.logical_or(flag_array, local_mask)
                 continue
 
             if "latitude" in variable_name:
-                local_mask = np.absolute(variable_data) > 90.0
+                local_mask = np.absolute(variable_data) > LAT_ABS_MAX
+                flag_array = np.logical_or(flag_array, local_mask)
+                continue
+
+            if "solar_zenith_angle" in variable_name:
+                local_mask = (variable_data < SZA_MIN) | (variable_data > SZA_MAX)
                 flag_array = np.logical_or(flag_array, local_mask)
                 continue
 
         dataset["invalid_data"].data = flag_array
+
+    def run_qa_bt_delta(self, dataset):
+        flag_array = dataset["invalid_data"].data
+
+        data_18V = dataset["amsre.brightness_temperature18V"].data
+        data_18H = dataset["amsre.brightness_temperature18H"].data
+        local_mask = (data_18V < data_18H)
+        flag_array = np.logical_or(flag_array, local_mask)
+
+        data_23V = dataset["amsre.brightness_temperature23V"].data
+        data_23H = dataset["amsre.brightness_temperature23H"].data
+        local_mask = (data_23V < data_23H)
+        flag_array = np.logical_or(flag_array, local_mask)
+
+        data_36V = dataset["amsre.brightness_temperature36V"].data
+        data_36H = dataset["amsre.brightness_temperature36H"].data
+        local_mask = (data_36V < data_36H)
+        flag_array = np.logical_or(flag_array, local_mask)
+
+        dataset["invalid_data"].data = flag_array
+        pass
--- test/dmi/sst/mw_oe/qa_processor_test.py
+++ test/dmi/sst/mw_oe/qa_processor_test.py
@@ -23,138 +23,236 @@ class QaProcessorTest(unittest.TestCase):
 
         self.qa_processor = QaProcessor()
 
-    def test_run_qa_pixel_data_quality_pass(self):
+    def test_run_qa_general_pixel_data_quality_pass(self):
         data = DefaultData.create_default_vector(NUM_MATCHES, np.int16, fill_value=0)
         self.dataset["amsre.pixel_data_quality10H"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_pixel_data_quality_fail(self):
+    def test_run_qa_general_pixel_data_quality_fail(self):
         data = DefaultData.create_default_vector(3, np.int16, fill_value=0)
         data[1] = 2
         self.dataset["amsre.pixel_data_quality10V"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertTrue(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_pixel_flag_fail(self):
+    def test_run_qa_general_pixel_flag_fail(self):
         data = DefaultData.create_default_vector(3, np.int16, fill_value=0)
         data[2] = 1
         self.dataset["insitu.sst_track_flag"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertTrue(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_brightness_temperature_pass(self):
+    def test_run_qa_general_brightness_temperature_pass(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 32.8
         data[1] = 33.9
         data[2] = 35.0
         self.dataset["amsre.brightness_temperature18V"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_brightness_temperature_fail(self):
+    def test_run_qa_general_brightness_temperature_fail(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = -1.8
         data[1] = 33.9
         data[2] = 498.55
         self.dataset["amsre.amsre.brightness_temperature36V"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertTrue(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertTrue(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_wind_speed_pass(self):
+    def test_run_qa_general_wind_speed_pass(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 11.8
         data[1] = -12.7
         data[2] = 8.445
         self.dataset["amsre.nwp.10m_east_wind_component"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_wind_speed_fail(self):
+    def test_run_qa_general_wind_speed_fail(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 11.8
         data[1] = -52.7
         data[2] = 108.445
         self.dataset["amsre.nwp.10m_north_wind_component"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertTrue(self.dataset["invalid_data"].data[1])
         self.assertTrue(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_longitude_pass(self):
+    def test_run_qa_general_longitude_pass(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 12.9
         data[1] = -14.7
         data[2] = 17.445
         self.dataset["amsre.longitude"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_longitude_fail(self):
+    def test_run_qa_general_longitude_fail(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 212.9
         data[1] = -214.7
         data[2] = 17.445
         self.dataset["amsre.longitude"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertTrue(self.dataset["invalid_data"].data[0])
         self.assertTrue(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_latitude_pass(self):
+    def test_run_qa_general_latitude_pass(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 12.9
         data[1] = -14.7
         data[2] = 17.445
         self.dataset["amsre.latitude"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertFalse(self.dataset["invalid_data"].data[2])
 
-    def test_run_qa_latitude_fail(self):
+    def test_run_qa_general_latitude_fail(self):
         data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
         data[0] = 12.9
         data[1] = -114.7
         data[2] = 90.445
         self.dataset["amsre.latitude"] = Variable(["matchup_count"], data)
 
-        self.qa_processor.run_qa(self.dataset)
+        self.qa_processor.run_qa_general(self.dataset)
 
         self.assertFalse(self.dataset["invalid_data"].data[0])
         self.assertTrue(self.dataset["invalid_data"].data[1])
+        self.assertTrue(self.dataset["invalid_data"].data[2])
+
+    def test_run_qa_general_SZA_pass(self):
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 12.9
+        data[1] = 14.7
+        data[2] = 17.445
+        self.dataset["amsre.solar_zenith_angle"] = Variable(["matchup_count"], data)
+
+        self.qa_processor.run_qa_general(self.dataset)
+
+        self.assertFalse(self.dataset["invalid_data"].data[0])
+        self.assertFalse(self.dataset["invalid_data"].data[1])
+        self.assertFalse(self.dataset["invalid_data"].data[2])
+
+    def test_run_qa_general_SZA_fail(self):
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 181.9
+        data[1] = -14.7
+        data[2] = 17.445
+        self.dataset["amsre.solar_zenith_angle"] = Variable(["matchup_count"], data)
+
+        self.qa_processor.run_qa_general(self.dataset)
+
+        self.assertTrue(self.dataset["invalid_data"].data[0])
+        self.assertTrue(self.dataset["invalid_data"].data[1])
+        self.assertFalse(self.dataset["invalid_data"].data[2])
+
+    def test_run_qa_general_avg_wind_speed_pass(self):
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 12.9
+        data[1] = 0.89
+        data[2] = 5.485
+        self.dataset["amsre.nwp.abs_wind_speed"] = Variable(["matchup_count"], data)
+
+        self.qa_processor.run_qa_general(self.dataset)
+
+        self.assertFalse(self.dataset["invalid_data"].data[0])
+        self.assertFalse(self.dataset["invalid_data"].data[1])
+        self.assertFalse(self.dataset["invalid_data"].data[2])
+
+    def test_run_qa_general_avg_wind_speed_fail(self):
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 22.9
+        data[1] = -0.89
+        data[2] = 5.485
+        self.dataset["amsre.nwp.abs_wind_speed"] = Variable(["matchup_count"], data)
+
+        self.qa_processor.run_qa_general(self.dataset)
+
+        self.assertTrue(self.dataset["invalid_data"].data[0])
+        self.assertTrue(self.dataset["invalid_data"].data[1])
+        self.assertFalse(self.dataset["invalid_data"].data[2])
+
+    def test_run_bt_delta_pass(self):
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 10.9
+        data[1] = 20.7
+        data[2] = 30.445
+        self.dataset["amsre.brightness_temperature18V"] = Variable(["matchup_count"], data)        
+        self.dataset["amsre.brightness_temperature23V"] = Variable(["matchup_count"], data)        
+        self.dataset["amsre.brightness_temperature36V"] = Variable(["matchup_count"], data)
+
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 10.0
+        data[1] = 20.0
+        data[2] = 30.0
+        self.dataset["amsre.brightness_temperature18H"] = Variable(["matchup_count"], data)
+        self.dataset["amsre.brightness_temperature23H"] = Variable(["matchup_count"], data)
+        self.dataset["amsre.brightness_temperature36H"] = Variable(["matchup_count"], data)
+
+        self.qa_processor.run_qa_bt_delta(self.dataset)
+
+        self.assertFalse(self.dataset["invalid_data"].data[0])
+        self.assertFalse(self.dataset["invalid_data"].data[1])
+        self.assertFalse(self.dataset["invalid_data"].data[2])
+
+    def test_run_bt_delta_fail(self):
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 10.9
+        data[1] = 20.7
+        data[2] = 30.445
+        self.dataset["amsre.brightness_temperature18V"] = Variable(["matchup_count"], data)
+        self.dataset["amsre.brightness_temperature23V"] = Variable(["matchup_count"], data)
+        self.dataset["amsre.brightness_temperature36V"] = Variable(["matchup_count"], data)
+
+        data = DefaultData.create_default_vector(3, np.float32, fill_value=0)
+        data[0] = 11.0
+        data[1] = 20.0
+        data[2] = 31.0
+        self.dataset["amsre.brightness_temperature18H"] = Variable(["matchup_count"], data)
+        self.dataset["amsre.brightness_temperature23H"] = Variable(["matchup_count"], data)
+        self.dataset["amsre.brightness_temperature36H"] = Variable(["matchup_count"], data)
+
+        self.qa_processor.run_qa_bt_delta(self.dataset)
+
+        self.assertTrue(self.dataset["invalid_data"].data[0])
+        self.assertFalse(self.dataset["invalid_data"].data[1])
         self.assertTrue(self.dataset["invalid_data"].data[2])
