bf956d97128a9906fe30ec043b6704a4cd396918
tom <tom.block@brockmann-consult.de>
2018-05-16 15:22:57 +0200

    split horiz and vert polarisation calculations, speed up

--- dmi/sst/mw_oe/fw_model.py
+++ dmi/sst/mw_oe/fw_model.py
@@ -34,6 +34,9 @@ COEFFS = np.array([[-822.280328393158, 0.031740043058786, -0.000556942697289299,
 
 DEG_TO_RAD = np.pi / np.float64(180.0)
 
+# background radiation [K]
+T_C = np.float64(2.7)
+
 EMISSIVITY_FY_V = np.array([0.9204, 0.9127, 0.9373, 0.9409, 0.9347], dtype=np.float64)
 EMISSIVITY_FY_H = np.array([0.7502, 0.7738, 0.8314, 0.8490, 0.8600], dtype=np.float64)
 EMISSIVITY_MY_V = np.array([0.9692, 0.9284, 0.8843, 0.8554, 0.7813], dtype=np.float64)
@@ -52,20 +55,16 @@ MC_ATM = np.array([[239.5, 2.1392, -0.04606, 0.00045711, -1.684e-006, 0.5, -0.11
 MC_ABS = np.array([[0.0078, 0.0303, 0.0007, 0, 1.2216], [0.0183, 0.0298, 0.0027, 0.006, 1.1795], [0.0556, 0.0288, 0.0113, 0.004, 1.0636], [0.0891, 0.0281, 0.0188, 0.002, 1.022],
                    [0.2027, 0.0261, 0.0425, -0.002, 0.9546]], dtype=np.float64)
 
+# Model coefficients for geometric optics
+#                   v-p r0   h-p r0     v-p r1   h-p r1     v-p tab--  h-p tab-- v-p r3 h-p r3
+MC_GEO = np.array([[-0.00027, 0.00054, -2.1e-005, 3.2e-005, -2.1e-005, -2.526e-005, 0, 0], [-0.00032, 0.00072, -2.9e-005, 4.4e-005, -2.1e-005, -2.894e-005, 8e-008, -2e-008],
+                   [-0.00049, 0.00113, -5.3e-005, 7e-005, -2.1e-005, -3.69e-005, 3.1e-007, -1.2e-007], [-0.00063, 0.00139, -7e-005, 8.5e-005, -2.1e-005, -4.195e-005, 4.1e-007, -2e-007],
+                   [-0.00101, 0.00191, -0.000105, 0.000112, -2.1e-005, -5.451e-005, 4.5e-007, -3.6e-007]], dtype=np.float64)
+
 
 class FwModel:
     light_speed = 3.00E10  # Speed of light, [cm/s]
     LAMBA = light_speed / (FREQ * 1E9)
-
-    # Model coefficients for geometric optics
-    #                   v-p r0   h-p r0     v-p r1   h-p r1     v-p tab--  h-p tab-- v-p r3 h-p r3
-    MC_GEO = np.array([[-0.00027, 0.00054, -2.1e-005, 3.2e-005, -2.1e-005, -2.526e-005, 0, 0], [-0.00032, 0.00072, -2.9e-005, 4.4e-005, -2.1e-005, -2.894e-005, 8e-008, -2e-008],
-                       [-0.00049, 0.00113, -5.3e-005, 7e-005, -2.1e-005, -3.69e-005, 3.1e-007, -1.2e-007], [-0.00063, 0.00139, -7e-005, 8.5e-005, -2.1e-005, -4.195e-005, 4.1e-007, -2e-007],
-                       [-0.00101, 0.00191, -0.000105, 0.000112, -2.1e-005, -5.451e-005, 4.5e-007, -3.6e-007]], dtype=np.float64)
-
-
-    # background radiation [K]
-    T_C = np.float64(2.7)
 
     def run(self, W, V, L, T_ow, C_is, F_MY, theta_d, sss, phi_rd):
         # convert angles to radians
@@ -84,11 +83,6 @@ class FwModel:
 
         T_is = calc_ice_temp(T_ow)
         T_ow = calc_open_water_temp(C_is, T_ow)
-
-        T_BV_FY = T_is * EMISSIVITY_FY_V  # brightness temperature from FY is (vertical pol)
-        T_BH_FY = T_is * EMISSIVITY_FY_H  # brightness temperature from FY is (horizontal pol)
-        T_BV_MY = T_is * EMISSIVITY_MY_V  # brightness temperature from MY is (vertical pol)
-        T_BH_MY = T_is * EMISSIVITY_MY_H  # brightness temperature from MY is (horizontal pol)
 
         T_S_mix = C_is * T_is + C_ow * T_ow  # temperature of mixed surfaces (ice and open water)
         T_L = (T_S_mix + 273.0) * 0.5  # temperature of the water droplets[K]; approximation: mean temp of surface and freezing level
@@ -120,7 +114,7 @@ class FwModel:
         tau = np.exp((-1.0 / cos_theta_r) * (A_0 + A_V + A_L))
 
         T_BU = T_U * (1.0 - tau)  # The upwelling effective air temperature; equation (24a)
-        #T_BD = T_D * (1.0 - tau)  # The downwelling effective air temperature; equation (24b)
+        # T_BD = T_D * (1.0 - tau)  # The downwelling effective air temperature; equation (24b)
 
         # --------------------------------
         # Dielectric Constant of Sea-water
@@ -150,65 +144,24 @@ class FwModel:
         sig_lamb = np.complex128(1j * sigma * self.LAMBA)
         epsilon = epsilon_R + (epsilon_S - epsilon_R) / (1 + np.power(lamb_r_ratio, 1.0 - ny)) - 2 * sig_lamb / self.light_speed
 
-        # equation (45b); horizontal pol.
         sin_theta_r = np.sin(theta_r)
         sqrt_eps_thet = np.sqrt(epsilon - sin_theta_r * sin_theta_r)
-        rho_H = (cos_theta_r - sqrt_eps_thet) / (cos_theta_r + sqrt_eps_thet)
-        # equation (45a); vertical pol.
-        rho_V = (epsilon * cos_theta_r - sqrt_eps_thet) / (epsilon * cos_theta_r + sqrt_eps_thet)
-
-        # Power reflectivity
-        R_0H = np.abs(rho_H) * np.abs(rho_H)  # equation (46); horizontal pol.
-        R_0V = np.abs(rho_V) * np.abs(rho_V) + (4.887E-8 - 6.108E-8 * np.power(T_ow - 273.0, 3.0))  # equation (46)+correction vertical pol.
-
-        # ------------------------------
-        # The wind-roughened Sea Surface
-        # ------------------------------
-        R_geoH = R_0H - (self.MC_GEO[:, 1] + self.MC_GEO[:, 3] * (theta_d - 53.0) + self.MC_GEO[:, 5] * (T_ow - 288.0) + self.MC_GEO[:, 7] * (theta_d - 53.0) * (T_ow - 288.0)) * W
-        R_geoV = R_0V - (self.MC_GEO[:, 0] + self.MC_GEO[:, 2] * (theta_d - 53.0) + self.MC_GEO[:, 4] * (T_ow - 288.0) + self.MC_GEO[:, 6] * (theta_d - 53.0) * (T_ow - 288.0)) * W
-
-        F_H = calc_F_horizontal(W, MC_M)
-        F_V = calc_F_vertical(W, MC_M)
-
-        R_H = (1.0 - F_H) * R_geoH  # equation (49) Composite reflectivity for open water; horizontal pol
-        R_V = (1.0 - F_V) * R_geoV  # equation (49) Composite reflectivity for open water; vertical pol
-
-        E_H = 1.0 - R_H  # surface emissivity for open water; Horizontal pol.; Equation (8);
-        E_V = 1.0 - R_V  # Surface emissivity for open water; Vertical pol; Equation (8);
-
-        # Emissivity for mixed surface
-        E_eff_H = C_ow * E_H + C_FY * EMISSIVITY_FY_H + C_MY * EMISSIVITY_MY_H
-        E_eff_V = C_ow * E_V + C_FY * EMISSIVITY_FY_V + C_MY * EMISSIVITY_MY_V
-
-        # Reflection coefficient for mixed surface
-        R_eff_H = 1.0 - E_eff_H
-        R_eff_V = 1.0 - E_eff_V
 
         # --------------------------------------------------
         # Atmospheric Radiation Scattered by the Sea Surface
         # --------------------------------------------------
         Delta_S2 = create_Delta_S2(W)
-        term = Delta_S2 - 70 * np.power(Delta_S2, 3)  # Term for equation (62a+b)
+        term_62 = Delta_S2 - 70 * np.power(Delta_S2, 3)  # Term for equation (62a+b)
 
-        OmegaH = (6.2 - 0.001 * np.square(37.0 - FREQ)) * term * np.square(tau)  # equation (62a); horizontal pol.
-        OmegaV = (2.5 + 0.018 * (37.0 - FREQ)) * term * np.power(tau, 3.4)  # equation (62b); vertical pol.
+        # ---------------------------------------------------------------------------------------------
+        # ----- HORIZONTAL ----------------------------------------------------------------------------
+        # ---------------------------------------------------------------------------------------------
+        T_BH = calc_horizontal_polarised_BT(C_FY, C_MY, C_ow, T_BU, T_D, T_is, T_ow, W, cos_theta_r, sqrt_eps_thet, tau, term_62, theta_d)
 
-        T_BOmegaH = ((1.0 + OmegaH) * (1.0 - tau) * (T_D - self.T_C) + self.T_C) * R_eff_H  # equation (61) horizontal pol.
-        T_BOmegaV = ((1.0 + OmegaV) * (1 - tau) * (T_D - self.T_C) + self.T_C) * R_eff_V  # equation (61) vertical pol.
-
-        # ------
-        # Output
-        # ------
-        T_BH_ow = E_H * T_ow  # Horizontal brightness temperature from open water
-        T_BV_ow = E_V * T_ow  # Vertical brightness temperature from open water
-
-        T_BH_overflade = C_ow * T_BH_ow + C_FY * T_BH_FY + C_MY * T_BH_MY  # Horizontal brightness temperature from mixed surface
-        T_BV_overflade = C_ow * T_BV_ow + C_FY * T_BV_FY + C_MY * T_BV_MY  # Vertical brightness temperature from mixed surface
-
-        # The result for the upwelling brightness temperature at the top of the
-        # atmosphere (i.e., the value observed by Earth-orbiting satellites)
-        T_BH = (T_BU + (tau * (T_BH_overflade + T_BOmegaH)))  # Equation (10); Horizontal pol.
-        T_BV = (T_BU + (tau * (T_BV_overflade + T_BOmegaV)))  # Equation (10); Vertical pol.
+        # ---------------------------------------------------------------------------------------------
+        # ----- VERTICAL ------------------------------------------------------------------------------
+        # ---------------------------------------------------------------------------------------------
+        T_BV = calc_vertical_polarised_BT(C_FY, C_MY, C_ow, T_BU, T_D, T_is, T_ow, W, cos_theta_r, epsilon, sqrt_eps_thet, tau, term_62, theta_d)
 
         # ------------------------------
         # sort output (exclude 53+89GHz)
@@ -292,8 +245,8 @@ def calc_sig_TS_TV(T_S_mix, T_V):
         return np.sign(delta) * 14.0
 
 
-@jit('float64[:](float64, float64[:, :])', nopython=True)
-def calc_F_horizontal(W, MC_M):
+@jit('float64[:](float64)', nopython=True)
+def calc_F_horizontal(W):
     W_1 = 7.0
     W_2 = 12.0
 
@@ -305,8 +258,8 @@ def calc_F_horizontal(W, MC_M):
         return MC_M[:, 3] * W - 0.5 * (MC_M[:, 3] - MC_M[:, 1]) * (W_2 + W_1)  # equation (60c)
 
 
-@jit('float64[:](float64, float64[:, :])', nopython=True)
-def calc_F_vertical(W, MC_M):
+@jit('float64[:](float64)', nopython=True)
+def calc_F_vertical(W):
     W_1 = 3.0
     W_2 = 12.0
 
@@ -332,3 +285,82 @@ def create_Delta_S2(W):
             delta_S2[i] = 0.069
 
     return delta_S2
+
+
+@jit('float64[:](float64, float64, float64, float64[:], float64[:], float64, float64, float64, float64, complex128[:], float64[:], float64[:], float64 )', nopython=True)
+def calc_horizontal_polarised_BT(C_FY, C_MY, C_ow, T_BU, T_D, T_is, T_ow, W, cos_theta_r, sqrt_eps_thet, tau, term_62, theta_d):
+    T_BH_FY = T_is * EMISSIVITY_FY_H  # brightness temperature from FY is (horizontal pol)
+    T_BH_MY = T_is * EMISSIVITY_MY_H  # brightness temperature from MY is (horizontal pol)
+
+    # equation (45b); horizontal pol.
+    rho_H = (cos_theta_r - sqrt_eps_thet) / (cos_theta_r + sqrt_eps_thet)
+
+    # Power reflectivity
+    R_0H = np.abs(rho_H) * np.abs(rho_H)  # equation (46); horizontal pol.
+
+    # ------------------------------
+    # The wind-roughened Sea Surface
+    # ------------------------------
+    R_geoH = R_0H - (MC_GEO[:, 1] + MC_GEO[:, 3] * (theta_d - 53.0) + MC_GEO[:, 5] * (T_ow - 288.0) + MC_GEO[:, 7] * (theta_d - 53.0) * (T_ow - 288.0)) * W
+    F_H = calc_F_horizontal(W)
+    R_H = (1.0 - F_H) * R_geoH  # equation (49) Composite reflectivity for open water; horizontal pol
+    E_H = 1.0 - R_H  # surface emissivity for open water; Horizontal pol.; Equation (8);
+
+    # Emissivity for mixed surface
+    E_eff_H = C_ow * E_H + C_FY * EMISSIVITY_FY_H + C_MY * EMISSIVITY_MY_H
+
+    # Reflection coefficient for mixed surface
+    R_eff_H = 1.0 - E_eff_H
+    OmegaH = (6.2 - 0.001 * np.square(37.0 - FREQ)) * term_62 * np.square(tau)  # equation (62a); horizontal pol.
+    T_BOmegaH = ((1.0 + OmegaH) * (1.0 - tau) * (T_D - T_C) + T_C) * R_eff_H  # equation (61) horizontal pol.
+
+    # ------
+    # Output
+    # ------
+    T_BH_ow = E_H * T_ow  # Horizontal brightness temperature from open water
+    T_BH_overflade = C_ow * T_BH_ow + C_FY * T_BH_FY + C_MY * T_BH_MY  # Horizontal brightness temperature from mixed surface
+
+    # The result for the upwelling brightness temperature at the top of the
+    # atmosphere (i.e., the value observed by Earth-orbiting satellites)
+    T_BH = (T_BU + (tau * (T_BH_overflade + T_BOmegaH)))  # Equation (10); Horizontal pol.
+    return T_BH
+
+
+@jit('float64[:](float64, float64, float64, float64[:], float64[:], float64, float64, float64, float64, complex128[:], complex128[:], float64[:], float64[:], float64 )', nopython=True)
+def calc_vertical_polarised_BT(C_FY, C_MY, C_ow, T_BU, T_D, T_is, T_ow, W, cos_theta_r, epsilon, sqrt_eps_thet, tau, term_62, theta_d):
+    T_BV_FY = T_is * EMISSIVITY_FY_V  # brightness temperature from FY is (vertical pol)
+    T_BV_MY = T_is * EMISSIVITY_MY_V  # brightness temperature from MY is (vertical pol)
+
+    # equation (45a); vertical pol.
+    rho_V = (epsilon * cos_theta_r - sqrt_eps_thet) / (epsilon * cos_theta_r + sqrt_eps_thet)
+
+    # Power reflectivity
+    R_0V = np.abs(rho_V) * np.abs(rho_V) + (4.887E-8 - 6.108E-8 * np.power(T_ow - 273.0, 3.0))  # equation (46)+correction vertical pol.
+
+    # ------------------------------
+    # The wind-roughened Sea Surface
+    # ------------------------------
+
+    R_geoV = R_0V - (MC_GEO[:, 0] + MC_GEO[:, 2] * (theta_d - 53.0) + MC_GEO[:, 4] * (T_ow - 288.0) + MC_GEO[:, 6] * (theta_d - 53.0) * (T_ow - 288.0)) * W
+    F_V = calc_F_vertical(W)
+    R_V = (1.0 - F_V) * R_geoV  # equation (49) Composite reflectivity for open water; vertical pol
+    E_V = 1.0 - R_V  # Surface emissivity for open water; Vertical pol; Equation (8);
+
+    # Emissivity for mixed surface
+    E_eff_V = C_ow * E_V + C_FY * EMISSIVITY_FY_V + C_MY * EMISSIVITY_MY_V
+
+    # Reflection coefficient for mixed surface
+    R_eff_V = 1.0 - E_eff_V
+    OmegaV = (2.5 + 0.018 * (37.0 - FREQ)) * term_62 * np.power(tau, 3.4)  # equation (62b); vertical pol.
+    T_BOmegaV = ((1.0 + OmegaV) * (1 - tau) * (T_D - T_C) + T_C) * R_eff_V  # equation (61) vertical pol.
+
+    # ------
+    # Output
+    # ------
+    T_BV_ow = E_V * T_ow  # Vertical brightness temperature from open water
+
+    # The result for the upwelling brightness temperature at the top of the
+    # atmosphere (i.e., the value observed by Earth-orbiting satellites)
+    T_BV_overflade = C_ow * T_BV_ow + C_FY * T_BV_FY + C_MY * T_BV_MY  # Vertical brightness temperature from mixed surface
+    T_BV = (T_BU + (tau * (T_BV_overflade + T_BOmegaV)))  # Equation (10); Vertical pol.
+    return T_BV
--- test/dmi/sst/mw_oe/fw_model_test.py
+++ test/dmi/sst/mw_oe/fw_model_test.py
@@ -2,11 +2,11 @@ import unittest
 
 import numpy as np
 
-from dmi.sst.mw_oe.fw_model import FwModel, create_Delta_S2, calc_F_vertical, MC_M, calc_F_horizontal, calc_sig_TS_TV, calc_T_V, calc_open_water_temp, calc_ice_temp, clamp_to_0_1
+from dmi.sst.mw_oe.fw_model import FwModel, create_Delta_S2, calc_F_vertical, calc_F_horizontal, calc_sig_TS_TV, calc_T_V, calc_open_water_temp, calc_ice_temp, clamp_to_0_1, \
+    calc_horizontal_polarised_BT, calc_vertical_polarised_BT
 
 
 class FwModelTest(unittest.TestCase):
-
     fw_model = None
 
     def setUp(self):
@@ -149,21 +149,21 @@ class FwModelTest(unittest.TestCase):
         self.assertAlmostEqual(14.0, calc_sig_TS_TV(321.68, 301.16), 8)
 
     def test_calc_F_horizontal(self):
-        F_H = calc_F_horizontal(6.0041, MC_M)
+        F_H = calc_F_horizontal(6.0041)
         self.assertAlmostEqual(0.0120082, F_H[0], 8)
         self.assertAlmostEqual(0.0120082, F_H[1], 8)
         self.assertAlmostEqual(0.017592013, F_H[2], 8)
         self.assertAlmostEqual(0.018492628, F_H[3], 8)
         self.assertAlmostEqual(0.019753489, F_H[4], 8)
 
-        F_H = calc_F_horizontal(8.32, MC_M)
+        F_H = calc_F_horizontal(8.32)
         self.assertAlmostEqual(0.01733696, F_H[0], 8)
         self.assertAlmostEqual(0.01733696, F_H[1], 8)
         self.assertAlmostEqual(0.0250100912, F_H[2], 8)
         self.assertAlmostEqual(0.0262389248, F_H[3], 8)
         self.assertAlmostEqual(0.0279495344, F_H[4], 8)
 
-        F_H = calc_F_horizontal(12.46, MC_M)
+        F_H = calc_F_horizontal(12.46)
         self.assertAlmostEqual(0.03676, F_H[0], 8)
         self.assertAlmostEqual(0.03676, F_H[1], 8)
         self.assertAlmostEqual(0.0472526, F_H[2], 8)
@@ -171,21 +171,21 @@ class FwModelTest(unittest.TestCase):
         self.assertAlmostEqual(0.050791, F_H[4], 8)
 
     def test_calc_F_vertical(self):
-        F_V = calc_F_vertical(2.95, MC_M)
+        F_V = calc_F_vertical(2.95)
         self.assertAlmostEqual(0.00059, F_V[0], 8)
         self.assertAlmostEqual(0.00059, F_V[1], 8)
         self.assertAlmostEqual(0.00413, F_V[2], 8)
         self.assertAlmostEqual(0.005251, F_V[3], 8)
         self.assertAlmostEqual(0.0075815, F_V[4], 8)
 
-        F_V = calc_F_vertical(6.0041, MC_M)
+        F_V = calc_F_vertical(6.0041)
         self.assertAlmostEqual(0.0045599829237222226, F_V[0], 8)
         self.assertAlmostEqual(0.0045599829237222226, F_V[1], 8)
         self.assertAlmostEqual(0.011393890899311112, F_V[2], 8)
         self.assertAlmostEqual(0.013454847155066667, F_V[3], 8)
         self.assertAlmostEqual(0.017656609146466665, F_V[4], 8)
 
-        F_V = calc_F_vertical(12.773, MC_M)
+        F_V = calc_F_vertical(12.773)
         self.assertAlmostEqual(0.0378837, F_V[0], 8)
         self.assertAlmostEqual(0.0378837, F_V[1], 8)
         self.assertAlmostEqual(0.04930928, F_V[2], 8)
@@ -195,8 +195,60 @@ class FwModelTest(unittest.TestCase):
     def test_create_Delta_S2(self):
         Delta_S2 = create_Delta_S2(6.0041)
 
-        self.assertAlmostEqual(0.011771282646377359 , Delta_S2[0], 8)
+        self.assertAlmostEqual(0.011771282646377359, Delta_S2[0], 8)
         self.assertAlmostEqual(0.014858456479049636, Delta_S2[1], 8)
         self.assertAlmostEqual(0.021080006317788141, Delta_S2[2], 8)
         self.assertAlmostEqual(0.024630728204465633, Delta_S2[3], 8)
         self.assertAlmostEqual(0.031341401999999997, Delta_S2[4], 8)
+
+    def test_calc_horizontal_polarised_BT(self):
+        C_FY = np.float64(0.0)
+        C_MY = np.float64(0.0)
+        C_OW = np.float64(1.0)
+        T_BU = np.asarray([5.715619033241641, 8.955752553740098, 43.31807669457342, 103.71525118258761, 63.50157111928275])
+        T_D = np.asarray([277.9079227797001, 281.7780772117912, 287.9742385877552, 287.9005203213625, 286.158209347942])
+        T_is = np.float64(273.15)
+        T_ow = np.float64(302.24587478637693)
+        W = np.float64(6.851329803466797)
+        cos_theta_r = np.float64(0.5731474681972756)
+        sqrt_eps_thet = np.asarray(
+            [(8.18068397903982 - 2.0265181632277742j), (7.835871119302394 - 2.2060117452380354j), (7.0089776625838 - 2.6333023300672145j), (6.510633581794293 - 2.781988836812065j),
+             (5.509650824400042 - 2.8611309670278877j)])
+        tau = np.asarray([0.9794182550786318, 0.9681861599331942, 0.849093784207826, 0.6365378317235144, 0.7768085298376742])
+        term_62 = np.asarray([0.013262662593043513, 0.016613918713500434, 0.02308027884647855, 0.026552120915210645, 0.03256184688580385])
+        theta_d = np.float64(55.029998779296875)
+
+        T_BH = calc_horizontal_polarised_BT(C_FY, C_MY, C_OW, T_BU, T_D, T_is, T_ow, W, cos_theta_r, sqrt_eps_thet, tau, term_62, theta_d)
+
+        self.assertAlmostEqual(84.55799647378079, T_BH[0], 8)
+        self.assertAlmostEqual(91.30348957877695, T_BH[1], 8)
+        self.assertAlmostEqual(143.45994630454751, T_BH[2], 8)
+        self.assertAlmostEqual(209.38622322820117, T_BH[3], 8)
+        self.assertAlmostEqual(174.85439033783067, T_BH[4], 8)
+
+    def test_calc_vertical_polarised_BT(self):
+        C_FY = np.float64(0.0)
+        C_MY = np.float64(0.0)
+        C_OW = np.float64(1.0)
+        T_BU = np.asarray([5.715619033241641, 8.955752553740098, 43.31807669457342, 103.71525118258761, 63.50157111928275])
+        T_D = np.asarray([277.9079227797001, 281.7780772117912, 287.9742385877552, 287.9005203213625, 286.158209347942])
+        T_is = np.float64(273.15)
+        T_ow = np.float64(302.24587478637693)
+        W = np.float64(6.851329803466797)
+        cos_theta_r = np.float64(0.5731474681972756)
+        epsilon = np.asarray([(63.488316478725764 - 33.15660934230131j), (57.20589035788824 - 34.572047446705184j), (42.8629886927603 - 36.91351442054196j), (35.320389727939684 - 36.22501989025095j),
+                              (22.841683776025178 - 31.527665182403382j)])
+        sqrt_eps_thet = np.asarray(
+            [(8.18068397903982 - 2.0265181632277742j), (7.835871119302394 - 2.2060117452380354j), (7.0089776625838 - 2.6333023300672145j), (6.510633581794293 - 2.781988836812065j),
+             (5.509650824400042 - 2.8611309670278877j)])
+        tau = np.asarray([0.9794182550786318, 0.9681861599331942, 0.849093784207826, 0.6365378317235144, 0.7768085298376742])
+        term_62 = np.asarray([0.013262662593043513, 0.016613918713500434, 0.02308027884647855, 0.026552120915210645, 0.03256184688580385])
+        theta_d = np.float64(55.029998779296875)
+
+        T_BH = calc_vertical_polarised_BT(C_FY, C_MY, C_OW, T_BU, T_D, T_is, T_ow, W, cos_theta_r, epsilon, sqrt_eps_thet, tau, term_62, theta_d)
+
+        self.assertAlmostEqual(173.50023752824694, T_BH[0], 8)
+        self.assertAlmostEqual(178.80704193888405, T_BH[1], 8)
+        self.assertAlmostEqual(210.09393012654223, T_BH[2], 8)
+        self.assertAlmostEqual(246.4076719622741, T_BH[3], 8)
+        self.assertAlmostEqual(231.17052656945668, T_BH[4], 8)
