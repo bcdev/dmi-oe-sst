corrected relative angle calculation

dmi/sst/mw_oe/preprocessor.py
@@ -7,6 +7,10 @@ from dmi.sst.mw_oe.pressure_processor import PressureProcessor
 from dmi.sst.util.default_data import DefaultData
 
 
+DEG_TO_RAD = np.pi / np.float64(180.0)
+RAD_TO_DEG = np.float64(180.0) / np.pi
+
+
 class Preprocessor:
     TO_SQUEEZE_NAMES = ["insitu.time", "insitu.lat", "insitu.lon", "insitu.sea_surface_temperature", "insitu.sst_depth", "insitu.sst_qc_flag", "insitu.sst_track_flag]"]
     TO_AVERAGE_NAMES = ["amsre.brightness_temperature6V", "amsre.brightness_temperature6H", "amsre.brightness_temperature10V", "amsre.brightness_temperature10H", "amsre.brightness_temperature18V",
@@ -218,7 +222,9 @@ class Preprocessor:
         if phi_sat < 0.0:
             phi_sat = phi_sat + 360.0
 
-        phi_w = 90.0 - np.arctan2(north_wind, east_wind)
+        north_wind_rad = north_wind * DEG_TO_RAD
+        east_wind_rad = east_wind * DEG_TO_RAD
+        phi_w = 90.0 - np.arctan2(north_wind_rad, east_wind_rad) * DEG_TO_RAD
         if phi_w < 0.0:
             phi_w = phi_w + 360.0
 

test/dmi/sst/mw_oe/mw_oe_sst_processor_iotest.py
@@ -33,16 +33,16 @@ class MwOeSstProcessorIoTest(unittest.TestCase):
         target_data = xr.open_dataset(self.expected_target_file, decode_times=False)
         try:
             variable = target_data["j"]
-            self.assertAlmostEqual(22.504837, variable.data[118, 0], 7)
-            self.assertAlmostEqual(13.011457, variable.data[118, 1], 6)
+            self.assertAlmostEqual(22.505741, variable.data[118, 0], 6)
+            self.assertAlmostEqual(13.014329, variable.data[118, 1], 6)
 
             variable = target_data["tb_rmse_ite"]
-            self.assertAlmostEqual(0.74872094, variable.data[119, 2], 8)
+            self.assertAlmostEqual(0.7479118, variable.data[119, 2], 7)
             self.assertTrue(np.isnan(variable.data[119, 3]))
 
             variable = target_data["tb_rmse_ite0"]
-            self.assertAlmostEqual(4.7365136, variable.data[120], 6)
-            self.assertAlmostEqual(6.5495081, variable.data[121], 7)
+            self.assertAlmostEqual(4.736254, variable.data[120], 6)
+            self.assertAlmostEqual(6.549006, variable.data[121], 7)
 
             variable = target_data["tb_chi_ite"]
             self.assertTrue(np.isnan(variable.data[122, 3]))
@@ -61,28 +61,28 @@ class MwOeSstProcessorIoTest(unittest.TestCase):
             self.assertTrue(np.isnan(variable.data[129, 6]))
 
             variable = target_data["dtb_ite0"]
-            self.assertAlmostEqual(17.06531, variable.data[130, 7], 6)
-            self.assertAlmostEqual(-0.030384755, variable.data[131, 8], 8)
+            self.assertAlmostEqual(17.065277, variable.data[130, 7], 6)
+            self.assertAlmostEqual(-0.029170848, variable.data[131, 8], 8)
 
             variable = target_data["TA0_ite0"]
-            self.assertAlmostEqual(158.90785, variable.data[132, 9], 5)
-            self.assertAlmostEqual(157.05952, variable.data[133, 0], 5)
+            self.assertAlmostEqual(158.90617, variable.data[132, 9], 5)
+            self.assertAlmostEqual(157.06029, variable.data[133, 0], 5)
 
             variable = target_data["j_ite0"]
-            self.assertAlmostEqual(9482.5068, variable.data[134], 4)
-            self.assertAlmostEqual(9604.834, variable.data[135], 4)
+            self.assertAlmostEqual(9482.511, variable.data[134], 3)
+            self.assertAlmostEqual(9604.835, variable.data[135], 4)
 
             variable = target_data["AK"]
-            self.assertAlmostEqual(0.8624385, variable.data[136, 1], 8)
+            self.assertAlmostEqual(0.86243814, variable.data[136, 1], 8)
             self.assertAlmostEqual(0.99990416, variable.data[137, 2], 8)
 
             variable = target_data["chisq"]
-            self.assertAlmostEqual(0.61820823, variable.data[138], 8)
-            self.assertAlmostEqual(0.68999028, variable.data[139], 8)
+            self.assertAlmostEqual(0.61924624, variable.data[138], 8)
+            self.assertAlmostEqual(0.6914389, variable.data[139], 7)
 
             variable = target_data["tb_rmse"]
-            self.assertAlmostEqual(0.30278438, variable.data[140], 8)
-            self.assertAlmostEqual(0.30981407, variable.data[141], 8)
+            self.assertAlmostEqual(0.30289936, variable.data[140], 8)
+            self.assertAlmostEqual(0.30990422, variable.data[141], 8)
 
             variable = target_data["p"]
             self.assertTrue(np.isnan(variable.data[142, 3]))
@@ -97,7 +97,7 @@ class MwOeSstProcessorIoTest(unittest.TestCase):
             self.assertTrue(np.isnan(variable.data[147, 5]))
 
             variable = target_data["dtb"]
-            self.assertAlmostEqual(-0.15789826, variable.data[148, 6], 8)
+            self.assertAlmostEqual(-0.1574104, variable.data[148, 6], 8)
             self.assertTrue(np.isnan(variable.data[149, 7]))
 
             variable = target_data["ds"]

test/dmi/sst/mw_oe/preprocessor_test.py
@@ -208,9 +208,9 @@ class PreprocessorTest(unittest.TestCase):
 
         variable = prep_data.variables["relative_angle"]
         self.assertEqual((11,), variable.shape)
-        self.assertAlmostEqual(279.19028, variable.data[0], 5)
-        self.assertAlmostEqual(280.10715, variable.data[1], 5)
-        self.assertAlmostEqual(281.05164, variable.data[2], 5)
+        self.assertAlmostEqual(278.02078, variable.data[0], 5)
+        self.assertAlmostEqual(279.01932, variable.data[1], 5)
+        self.assertAlmostEqual(280.01834, variable.data[2], 5)
 
     def test_extract_ascending_descending(self):
         data = ["AMSR_E_L2A_BrightnessTemperatures_V12_200807110947_D.hdf", "AMSR_E_L2A_BrightnessTemperatures_V12_200807111125_D.hdf", "AMSR_E_L2A_BrightnessTemperatures_V12_200807112208_A.hdf",
@@ -324,7 +324,8 @@ class PreprocessorTest(unittest.TestCase):
         north_wind = np.float32([1.147087, 9.204754, 4.513235, -0.228445, 4.386336, -2.5637])
         east_wind = np.float32([-5.89346, 0.18209, -2.37729, -7.10264, -5.17445, 10.23945])
 
-        expecetd = np.float32([193.0293607711792, 286.39101684093475, 111.65561056137085, 102.46056699752808, 282.59845, 42.504668653011322])
+        #expecetd = np.float32([193.0293607711792, 286.39101684093475, 111.65561056137085, 102.46056699752808, 282.59845, 42.504668653011322])
+        expecetd = np.float32([190.13147785352797, 284.86707050140893, 109.63588316848497, 105.51573735228001, 280.2025586622923, 42.74571816021417])
 
         for i in range(0, len(phi_sat)):
             phi_rel = self.preprocessor.calculate_relative_angle(phi_sat[i], north_wind[i], east_wind[i])